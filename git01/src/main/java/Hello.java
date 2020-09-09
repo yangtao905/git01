909faac7baa65163bd3dfb57b33ed7b074b7be3c
LeftDearDream <398670091@qq.com>
2020-09-09 15:20:11 +0800

    aaa

--- git01/src/main/java/Hello.java
+++ git01/src/main/java/Hello.java
@@ -9,6 +9,10 @@ public class Hello {
         System.out.println("hello");
         System.out.println("hi");
         System.out.println("hello world");
+        System.out.println("hello java");
+
+
+
 
 
     }
