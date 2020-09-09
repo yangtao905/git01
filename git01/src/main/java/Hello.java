467cf7c03fa29425a23bf75817077e5b0bf04930
LeftDearDream <398670091@qq.com>
2020-09-09 14:37:57 +0800

    创建了一个项目

---
+++ git01/src/main/java/Hello.java
@@ -0,0 +1,18 @@
+
+/**
+ * @author yangtao
+ * @since 2020-09-09 14:32
+ */
+public class Hello {
+
+    public void hello(){
+        System.out.println("hello");
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("");
+
+    }
+
+}
---
+++ git01/src/test/java/com/example/git01/Git01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.git01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Git01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
